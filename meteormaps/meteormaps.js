ec98c267589e10e67eeb5371de4c8a298eef8ef3
Charis Rooda <charis@imakewebsites.nl>
2015-03-26 13:18:28 +0800

    clean up.

--- meteormaps/meteormaps.js
+++ meteormaps/meteormaps.js
@@ -1,25 +1,11 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 
   Meteor.startup(function() {
     GoogleMaps.load();
   });
 
   Template.body.helpers({
+  	// preset of map options
     exampleMapOptions: function() {
       // Make sure the maps API has loaded
       if (GoogleMaps.loaded()) {
